3b0eb494b12f851df89098b03abf0835bf82d1e1
lokesh-dadhich <165267716+lokesh-dadhich@users.noreply.github.com>
2026-05-18 13:38:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,13 +82,33 @@ module "eks" {
 
   enable_irsa = true
 
+  # This gives admin access to the IAM role/user that creates the cluster
+  enable_cluster_creator_admin_permissions = true
+
+  # This gives Jenkins IAM role access to EKS
+  access_entries = {
+    jenkins = {
+      principal_arn = "arn:aws:iam::809338888549:role/terraform"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
   eks_managed_node_groups = {
     default = {
       desired_size = 2
       min_size     = 1
       max_size     = 3
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.medium"]
 
       ami_type = "AL2023_x86_64_STANDARD"
     }
